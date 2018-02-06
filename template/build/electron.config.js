d92279e45c2859d62b670549b4a87f18737feaed
allen-mo <735016387@qq.com>
2018-02-06 14:40:13 +0800

    package ignore

--- template/build/electron.config.js
+++ template/build/electron.config.js
@@ -9,7 +9,7 @@ module.exports = {
   asar: true,
   dir: path.join(__dirname, '../'),
   icon: path.join(__dirname, '../platform/icons/icon'),
-  ignore: /(^\/(src|test|\.[a-z]+|README|yarn|static|dist\/web))|\.gitkeep/,
+  ignore: /(^\/(src|test|\.[a-z]+|README|node_modules|build|yarn|static|dist\/(sit|prod)))|\.gitkeep/,
   out: path.join(__dirname, '../platform'),
   overwrite: true,
   platform: process.env.BUILD_TARGET || 'all'
